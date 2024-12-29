37c44964cbfbf09953051b70567eb4039ab27e46
mohits-git <152606488+mohits-git@users.noreply.github.com>
2024-12-29 11:13:18 +0530

    feat: add base_url cli argument to start our app

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+  args := os.Args[1:]
+  if len(args) == 0 {
+    fmt.Println("no website provided")
+    os.Exit(1)
+  }
+
+  if len(args) > 1 {
+    fmt.Println("too many arguments provided")
+    os.Exit(1)
+  }
+
+  base_url := args[0]
+  fmt.Println("starting crawl of:", base_url)
 }
